e61c854023044107270339c6b79e319b839a09f1
CHINTAN PATEL <83652871+Code-Runnerss@users.noreply.github.com>
2025-05-20 10:14:32 +0000

    operators

--- 01_basics/03_conversationOperations.js
+++ 01_basics/03_conversationOperations.js
@@ -1,42 +1,85 @@
-let score = '33abc';
-let score2 = 33;
-let score3 = null;
-let score4 = NaN;
-let score5 = undefined;
-let ans = true;
+// let score = '33abc';
+// let score2 = 33;
+// let score3 = null;
+// let score4 = NaN;
+// let score5 = undefined;
+// let ans = true;
 
-// console.log(typeof score); // string
-// console.log(typeof (score2)); // number
+// // console.log(typeof score); // string
+// // console.log(typeof (score2)); // number
 
-let valueInNumer = Number(score);
-// console.log(typeof valueInNumer); // number 
-// console.log(valueInNumer); // NaN 
-// console.log(typeof score3); // object
-// console.log(score3); // null
-// console.log(typeof score4); // number
-// console.log(score5); //  undefined
-// console.log(typeof score5); // undefined
-// console
-// console.log(typeof ans); // boolean
+// let valueInNumer = Number(score);
+// // console.log(typeof valueInNumer); // number 
+// // console.log(valueInNumer); // NaN 
+// // console.log(typeof score3); // object
+// // console.log(score3); // null
+// // console.log(typeof score4); // number
+// // console.log(score5); //  undefined
+// // console.log(typeof score5); // undefined
+// // console
+// // console.log(typeof ans); // boolean
 
-// '33' -> 33
-// '33abc' -> NaN
-// 'true' -> 1 ; false -> 0
+// // '33' -> 33
+// // '33abc' -> NaN
+// // 'true' -> 1 ; false -> 0
 
-let isLoggedIn = true;
-let booleanIsLoggedIn = Boolean(isLoggedIn);
+// let isLoggedIn = true;
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
 
-console.log(booleanIsLoggedIn); // true
+// console.log(booleanIsLoggedIn); // true
 
-let isLoggedIn1 = '';
-let booleanIsLoggedIn1 = Boolean(isLoggedIn1);
-console.log(booleanIsLoggedIn1); // false
+// let isLoggedIn1 = '';
+// let booleanIsLoggedIn1 = Boolean(isLoggedIn1);
+// console.log(booleanIsLoggedIn1); // false
 
-let isLoggedIn2 = 'abc';
-let booleanIsLoggedIn2 = Boolean(isLoggedIn2);
-console.log(booleanIsLoggedIn2); // true
+// let isLoggedIn2 = 'abc';
+// let booleanIsLoggedIn2 = Boolean(isLoggedIn2);
+// console.log(booleanIsLoggedIn2); // true
 
-let someNumber = 33;
-let stringNumber = String(someNumber);
-console.log(stringNumber); // '33'
-console.log(typeof stringNumber); // string
+// let someNumber = 33;
+// let stringNumber = String(someNumber);
+// console.log(stringNumber); // '33'
+// console.log(typeof stringNumber); // string
+
+// operatiors
+
+// let vale = 3;
+// let negValue = -vale;   
+// console.log(negValue); // -3
+
+// console.log(3 + 4); // 7
+// console.log(3 - 4); // -1
+// console.log(3 * 4); // 12
+// console.assert(3 / 4); // 0.75
+// console.log(3 % 4); // 3    
+// console.log(3 ** 4); // 81
+
+// let str1 = 'Hello';
+// let str2 = ' World';
+// let str3 = str1 + str2;
+// console.log(str3); // Hello World   
+
+// console.log('1' + 3); // '13'   
+// console.log(1 + '3'); // '13'
+// console.log(1 + 3 + '1'); // '41'
+// console.log(1 + '3' + 1); // '131'
+// console.log('1' + 3 + 1); // '131'
+
+// console.log(3 + 4 * 5 % 3); // 2
+// console.log((3 + 4) * 5 % 3); // 2
+
+// console.log(+true); // 1
+// console.log(true+); not valid
+// console.log(+false); // 0
+// console.log(+""); // 0
+
+let num1 , num2 ,num3;
+
+num1 = num2 = num3 = 3 + 3;    
+
+let gameCounter = 100;  
+gameCounter++;
+console.log(gameCounter); // 101
+
+++gameCounter;
+console.log(gameCounter); // 101
