b00a29b1a795d3472764cebbf795408524d99cb2
CHINTAN PATEL <83652871+Code-Runnerss@users.noreply.github.com>
2025-05-20 07:14:13 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+// Data Types in JavaScript
+alert( 3 + 3 ); // Display a message in an alert box  // we are using nodejs not browser
+
+console.log('chitnan'); // Display a message in the console
+let name ='chitnan'; // Declare a variable and assign a value string
+let age = 26; // Declare a variable and assign a value number
+let isLogin = false; // Declare a variable and assign a value bolean
+let state;
+
+
+//NUMBER -> 2 TO  power 53
+// BigInt 
+//string -> 'chitnan' , "chitnan" , `chitnan`
+// boolean -> true , false
+// null -> empty value standalone value
+// undefined -> variable is declared but not assigned
+// symbol -> unique value
+// object -> collection of key value pair
+// typeof -> to check the type of variable
+
+console.log(typeof 'chitnan'); // Display a message in the console    
+console.log(typeof age); // Display a message in the console
+console.log(typeof null); // Display a message in the console object
+console.log(typeof undefined); // Display a message in the console undefined
